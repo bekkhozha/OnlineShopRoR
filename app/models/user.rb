b9d27012e66c68dd30496f437c9efc2e723de3ec
bekkhozha <bekkozha01@gmail.com>
2017-06-27 20:41:37 +0600

    [ADD] - added admin rules, for put admin previligious to user use CMD or Terminal

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :products
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  scope :get_admin, -> {where(admin: true)}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
