79ae744244042e293eabaca637942fadf882a9b1
bekkhozha <bekkozha01@gmail.com>
2017-06-29 23:05:10 +0600

    [ADD]- added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.delete_all
-User.create! id:1 ,phone:"+77777777777",password:"adminadm",admin:true, email:"admin@admin.adm"
+User.create! id:1, email:"admin@admin.com", password:"adminadm", admin:true
